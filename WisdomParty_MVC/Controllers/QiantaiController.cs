443e652bf697cdd8be07c9add1f41b076c96facf
qiaozhou0307 <qiaozhou0307@163.com>
2020-11-11 19:12:17 +0800

    哈哈哈

--- WisdomParty_MVC/Controllers/QiantaiController.cs
+++ WisdomParty_MVC/Controllers/QiantaiController.cs
@@ -7,8 +7,18 @@ using System.Web.Mvc;
 namespace WisdomParty_MVC.Controllers
 {
     public class QiantaiController : Controller
-    {
-        // GET: Qiantai
+    { 
+        //主页
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        //登录
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
         public ActionResult Index()
         {
             return View();
